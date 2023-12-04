2d7b6bac861a3b909d4109d27decbe435106b570
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-04 20:46:03 +0530

    doing basic networking using boost::beast

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,25 @@
-#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
 #include <iostream>
+#include <string>
+#include <thread>
 
-using boost::multiprecision::cpp_dec_float_50;
+using tcp = boost::asio::ip::tcp;
 
-template <typename T> inline T area_of_a_circle(T r) {
-    using boost::math::constants::pi;
-    return pi<T>() * r * r;
-}
+int main(int argc, char* argv[]) {
+    auto const address = boost::asio::ip::make_address("127.0.0.1");
+    auto const ip = static_cast<unsigned short>(std::atoi("8082"));
 
-int main() {
-    float radius_f = 123.0 / 100;
-    float area_f = area_of_a_circle(radius_f);
+    boost::asio::io_context ioc{1};
 
-    double radius_d = 123.0 / 100;
-    double area_d = area_of_a_circle(radius_d);
+    tcp::acceptor acceptor{ioc, {address, ip}};
 
-    cpp_dec_float_50 r_mp = 123.0 / 100;
-    cpp_dec_float_50 area_mp = area_of_a_circle(r_mp);
+    while (true) {
+        tcp::socket socket{ioc};
+        acceptor.accept(socket);
+        std::cout << "socket accepted"
+                  << "\n";
+    }
 
-    std::cout << "Float: " << std::setprecision(std::numeric_limits<float>::digits10) << area_f
-         << std::endl;
-    std::cout << "Double: " << std::setprecision(std::numeric_limits<double>::digits10)
-         << area_d << std::endl;
-    std::cout << "Boost Multiprecision: "
-         << std::setprecision(std::numeric_limits<cpp_dec_float_50>::digits10) << area_mp
-         << std::endl;
     return 0;
 }
