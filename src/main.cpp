5b57641d3595d5a52673fdf9495684e0146dae06
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-06 11:06:22 +0530

    use cpm (#1)
    * use cpm
    
    * update
    
    * push
    
    * change to main
    
    * switch to gtkmm
    
    * updates
    
    * update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
-#include <gtkmm.h>
+#include <gtkmm/application.h>
+#include <gtkmm/togglebutton.h>
+#include <gtkmm/window.h>
+#include <fmt/core.h>
 
-class MyWindow : public Gtk::Window {
+class MainWindow : public Gtk::Window {
   public:
-    MyWindow();
+    MainWindow();
+
+  private:
+    Gtk::ToggleButton toggleButton;
+    void onToggleClicked();
 };
 
-MyWindow::MyWindow() {
-    set_title("Basic application");
+MainWindow::MainWindow() {
+    set_title("Toggle Button Demo");
     set_default_size(200, 200);
+
+    toggleButton.set_label("On/Off");
+    toggleButton.signal_toggled().connect(
+        sigc::mem_fun(*this, &MainWindow::onToggleClicked));
+
+    add(toggleButton);
+    show_all();
+}
+
+void MainWindow::onToggleClicked() {
+    bool isToggled = toggleButton.get_active();
+
+    if (isToggled) {
+        fmt::print("Toggle button is on\n");
+    } else {
+        fmt::print("Toggle button is off\n");
+    }
 }
 
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.gtkmm.examples.base");
-
-    return app->make_window_and_run<MyWindow>(argc, argv);
+    Glib::RefPtr<Gtk::Application> app =
+        Gtk::Application::create(argc, argv, "com.example.gtkmm");
+    MainWindow window;
+    return app->run(window);
 }
